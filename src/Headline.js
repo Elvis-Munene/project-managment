095333bb911e289ccbe0a5df346e12c28323928a
Elvis-Munene <elvismuneneg@gmail.com>
2022-11-24 17:06:49 +0300

    imports react icons

--- src/Headline.js
+++ src/Headline.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { BsStar } from 'react-icons/bs'
+
 
 export default function Headline() {
   return (
@@ -9,7 +11,7 @@ export default function Headline() {
       <div>
     <h1>One app to replace them all</h1>
     <p>All of your work in one place: Tasks, Docs,Chat,Goals, & more</p>
-    <form class="p-3">
+    <form >
     <div>
     <input type="email" placeholder="Enter your Email Address" />
     </div>
@@ -26,6 +28,9 @@ export default function Headline() {
    
     <img class='img-fluid w-50 h-50'    src='https://www.ucl.ac.uk/translational-research/sites/translational_research/files/styles/large_image/public/project_management_lolly_graphic_final.png?itok=lEgYs3Vp' alt='side image'/>
     </div>
+    </section>
+    <section class="d-flex p-1">
+    <BsStar/> <BsStar/> <BsStar/> <BsStar/> <BsStar/><p>Based on 10,000+reviews on</p>
     </section>
    
 
