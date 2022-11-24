2461db9e82dedeadee337bd6e759410185f490b1
Elvis-Munene <elvismuneneg@gmail.com>
2022-11-24 16:42:36 +0300

    modifies the headline component

--- src/Headline.js
+++ src/Headline.js
@@ -4,12 +4,31 @@ export default function Headline() {
   return (
     <div class="bg-dark text-light p-5 text-center">
     <div class="container">
-    <section>
+   <section>
+   <div class="d-flex">
+      <div>
     <h1>One app to replace them all</h1>
     <p>All of your work in one place: Tasks, Docs,Chat,Goals, & more</p>
+    <form class="p-3">
+    <div>
+    <input type="email" placeholder="Enter your Email Address" />
+    </div>
+    </form>
+  
+    <div class="d-flex" >
     <button type="button" class="btn btn-primary">Get Started</button>
-    </section>
+    <p class="p-2">FREE FOREVER <span> <p>NO CREDIT CARD</p></span></p>
     
+    </div>
+   
+    
+    </div>
+   
+    <img class='img-fluid w-50 h-50'    src='https://www.ucl.ac.uk/translational-research/sites/translational_research/files/styles/large_image/public/project_management_lolly_graphic_final.png?itok=lEgYs3Vp' alt='side image'/>
+    </div>
+    </section>
+   
+
     </div>
     
     </div>
