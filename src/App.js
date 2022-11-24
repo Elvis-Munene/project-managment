6cf8cef2178eff2f70d9b891aea54e79b84001a6
Elvis-Munene <elvismuneneg@gmail.com>
2022-11-24 04:06:28 +0300

    adds the headline component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
+import Headline from './Headline';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <Headline/>
     </div>
   );
 }
---
+++ src/Headline.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Headline() {
+  return (
+    <div class="bg-dark text-light p-5 text-center">
+    <div class="container">
+    <section>
+    <h1>One app to replace them all</h1>
+    <p>All of your work in one place: Tasks, Docs,Chat,Goals, & more</p>
+    <button type="button" class="btn btn-primary">Get Started</button>
+    </section>
+    
+    </div>
+    
+    </div>
+  )
+}
